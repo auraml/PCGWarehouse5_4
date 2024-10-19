bbbd1bba263cec0256854ced32cd4aa160d86901
Arjun Gupta <arjuntheprogrammer@gmail.com>
2024-10-19 15:21:34 +0000

    fixed duplicate key issue in generate scene json

---
+++ Scripts/check_box_key.py
@@ -0,0 +1,14 @@
+import os
+import json
+
+path = "D:\\UnrealProjects\\PCGWarehouse5_4\\Scripts\\exported_scenes"
+for root, dirs, files in os.walk(path):
+    for file in files:
+        if file.endswith(".json"):
+            with open(os.path.join(root, file), "r") as f:
+                data = json.load(f)
+                if not any("Boxx" in key for key in data.keys()):
+                    print()
+                    print(file)
+                    for key in data.keys():
+                        print("-", key, ":\t", len(data[key]))
--- Scripts/export_scene.py
+++ Scripts/export_scene.py
@@ -3,6 +3,8 @@ import json
 import random
 import time
 
+scene_data = {}
+
 
 def get_arrary_from_vector(input_vector):
     return [input_vector.x, input_vector.y, input_vector.z]
@@ -25,9 +27,9 @@ def get_hism_instance_transforms(hism_component):
             'sizes': scale
         }
 
-        if static_mesh_name not in instance_transforms:
-            instance_transforms[static_mesh_name] = []
-        instance_transforms[static_mesh_name].append(instance_data)
+        if static_mesh_name not in scene_data:
+            scene_data[static_mesh_name] = []
+        scene_data[static_mesh_name].append(instance_data)
 
     return instance_transforms
 
@@ -37,25 +39,36 @@ def get_rack_actor(actors):
         actor_name = actor.get_actor_label()  # Get the actor's name
         if actor_name == "BP_Racks":
             return actor
-
     return None
 
 
 def get_scene_data(rack_actor):
-    scene_data = []
-
     # get all HISMC of rack_actor
     hismc = rack_actor.get_components_by_class(
         unreal.HierarchicalInstancedStaticMeshComponent)
     for component in hismc:
-        scene_data.append(get_hism_instance_transforms(component))
-    return scene_data
+        get_hism_instance_transforms(component)
 
 
 if __name__ == "__main__":
-    row = random.randint(1, 10)
-    col = random.randint(1, 10)
-    print(row, col)
+    previous_config_path = "D:\\UnrealProjects\\PCGWarehouse5_4\\Scripts\\prev_config.json"
+    prev_row = 1
+    prev_col = 1
+    with open(previous_config_path, 'r') as file:
+        data = json.load(file)
+        prev_row = data['rows']
+        prev_col = data['columns']
+
+    current_row = random.randint(1, 10)
+    current_col = random.randint(1, 10)
+
+    # write current data to json file
+    data = {
+        "rows": current_row,
+        "columns": current_col
+    }
+    with open(previous_config_path, 'w') as file:
+        json.dump(data, file)
 
     rack_actor = None
     editor_actor_subsystem = unreal.get_editor_subsystem(
@@ -63,12 +76,12 @@ if __name__ == "__main__":
     actors = editor_actor_subsystem.get_all_level_actors()
     rack_actor = get_rack_actor(actors)
 
-    rack_actor.set_editor_property("Rack Columns", int(col))
-    rack_actor.set_editor_property("Rack Rows", int(row))
+    rack_actor.set_editor_property("Rack Rows", int(current_row))
+    rack_actor.set_editor_property("Rack Columns", int(current_col))
 
-    scene_data = get_scene_data(rack_actor)
+    get_scene_data(rack_actor)
 
     timestamp = int(time.time() * 1000)
-    file_name = f"D:\\UnrealProjects\\PCGWarehouse5_4\\Scripts\\exported_scenes\\scene_data_{timestamp}.json"
+    file_name = f"D:\\UnrealProjects\\PCGWarehouse5_4\\Scripts\\exported_scenes\\{timestamp}_{prev_row}_{prev_col}.json"
     with open(file_name, 'w') as file:
         json.dump(scene_data, file)
